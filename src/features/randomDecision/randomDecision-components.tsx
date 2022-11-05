dd94da47857f12071fa7ece9c6fe56d2e3e93eae
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-05 10:43:10 +0800

    abstracted onSubmit

--- src/features/randomDecision/randomDecision-components.tsx
+++ src/features/randomDecision/randomDecision-components.tsx
@@ -1,8 +1,7 @@
 import { DecisionTitle } from "../../common/components/branding";
 import { IDecision } from "../../common/types/decision-types";
 import ChoicesForm from "../choiceForm/choiceForm-components";
-import RandomDecision from "./randomDecision-api";
-import useRandomDecisionForm from "./randomDecision-hooks";
+import RandomDecisionAPI from "./randomDecision-api";
 
 export function RandomDecisionTitle() {
   return <DecisionTitle title="Random Decision" />;
@@ -11,18 +10,9 @@ export function RandomDecisionTitle() {
 export function RandomDecisionForm() {
   return (
     <ChoicesForm
-      onSubmit={async (value: IDecision) => {
-        try {
-          value.createdAt = "2022-11-03T14:55:15.063Z";
-          value.updatedAt = "2022-11-03T14:55:15.063Z";
-          const postResponse = await RandomDecision.post(value);
-          const ID = postResponse.id;
-          const res = await RandomDecision.getDecide(ID as number);
-          alert(JSON.stringify(res));
-        } catch (error) {
-          alert(error);
-        }
-      }}
+      onSubmit={async (value: IDecision) =>
+        await RandomDecisionAPI.onSubmit(value)
+      }
     />
   );
 }
