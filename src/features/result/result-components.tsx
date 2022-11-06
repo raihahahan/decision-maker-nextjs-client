43db477b29bc9755c65232470aac4023eb0f55f3
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-06 20:20:34 +0800

    modified result components

--- src/features/result/result-components.tsx
+++ src/features/result/result-components.tsx
@@ -1,33 +1,62 @@
-import { Paper, Text } from "@mantine/core";
+import { Button, Paper, Text } from "@mantine/core";
 import { IWeightedResult } from "../../common/types/decision-types";
+import { breakpoints } from "../theme/theme-data";
 
-export function Result({ data }: { data: IWeightedResult }) {
+export function Result({
+  data,
+  index,
+}: {
+  data: IWeightedResult;
+  index?: number;
+}) {
+  const { lg } = breakpoints;
   return (
-    <Paper shadow="xs" p="lg" style={{ margin: 10 }}>
-      <div>
-        <Text>{data.name}</Text>
-        <Text>Weight: </Text>
-
-        {data.totalWeight}
-      </div>
+    <Paper
+      shadow="xs"
+      p="lg"
+      style={{
+        margin: 10,
+        width: "40vw",
+        maxWidth: breakpoints.sm,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {index != undefined && typeof index == "number" && (
+        <div>
+          <h3>
+            {index + 1}: {data?.name}
+          </h3>
+        </div>
+      )}
+      {index == undefined && <h3>{data?.name}</h3>}
     </Paper>
   );
 }
 
-export function ResultList({
-  resultData,
-  decisionName,
-}: {
-  resultData: IWeightedResult[];
-  decisionName: string;
-}) {
+export function ResultList({ resultData }: { resultData: IWeightedResult[] }) {
   return (
     <div>
-      <Text>Decision Name: </Text>
-      <Text>{decisionName}</Text>
-      {resultData.map((item) => {
-        return <Result data={item} />;
+      {resultData.map((item, index) => {
+        return <Result data={item} index={index} />;
       })}
     </div>
+  );
+}
+
+export function EditDecisionButton({ onClick }: { onClick: () => void }) {
+  return (
+    <Button style={{ margin: 10 }} onClick={onClick} color="green" size="lg">
+      Edit
+    </Button>
+  );
+}
+
+export function CreateNewDecisionButton({ onClick }: { onClick: () => void }) {
+  return (
+    <Button style={{ margin: 10 }} onClick={onClick} color="green" size="lg">
+      Create new decision
+    </Button>
   );
 }
