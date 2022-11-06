144cc6037c4e9deaeaa5fc6fcf6270bc78f98e8a
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-06 20:17:46 +0800

    added http methods to DAL class

--- src/common/domains/DAL.ts
+++ src/common/domains/DAL.ts
@@ -1,12 +1,13 @@
-import { NextRouter } from "next/router";
-import { IChoice, IDecision, IWeightedResult } from "../types/decision-types";
+import { IDecision, IFinalResult } from "../types/decision-types";
 
 // DAL: Data Access Layer
 
 abstract class DAL<T> {
   abstract get(): Promise<T[]>;
+  abstract getById(id: number): Promise<T>;
   abstract post(input: IDecision): Promise<IDecision>;
-  abstract decide(id: number): Promise<IWeightedResult[]>;
+  abstract put(id: number, value: IDecision): Promise<void>;
+  abstract decide(id: number): Promise<IFinalResult>;
 }
 
 export default DAL;
