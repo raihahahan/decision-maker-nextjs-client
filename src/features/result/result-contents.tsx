9682f8d91efccc86f5c7e79612ec0dcc6e1508a5
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-06 20:20:54 +0800

    modified result contents

--- src/features/result/result-contents.tsx
+++ src/features/result/result-contents.tsx
@@ -1,9 +1,60 @@
+import { Button, Text } from "@mantine/core";
+import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
-import { IWeightedResult } from "../../common/types/decision-types";
-import { ResultList } from "./result-components";
+import {
+  DecisionTypes,
+  IFinalResult,
+  IWeightedResult,
+} from "../../common/types/decision-types";
+import { RandomAskAgainButton } from "../randomDecision/randomDecision-components";
+import { useGlobalMediaQuery } from "../theme/theme-hooks";
+import {
+  CreateNewDecisionButton,
+  EditDecisionButton,
+  Result,
+  ResultList,
+} from "./result-components";
 import useResult from "./result-hooks";
 
-export default function ResultContents({ query }: { query: ParsedUrlQuery }) {
-  const { weightedResult, decisionName } = useResult(query);
-  return <ResultList resultData={weightedResult} decisionName={decisionName} />;
+export default function ResultContents({
+  data,
+  type,
+}: {
+  data: IFinalResult;
+  type: DecisionTypes;
+}) {
+  const { weightedResults, decisionName } = data;
+  const { onClickEdit, onClickCreateNew } = useResult();
+  const { sm, md } = useGlobalMediaQuery();
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: 20,
+        width: "90vw",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>
+        <b>Decision Name:</b> {decisionName}
+      </h1>
+      <h2>Random choice:</h2>
+      <Result data={weightedResults[0]} />
+      <br />
+      <h2>Ranked choice:</h2>
+      <ResultList resultData={weightedResults} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: md ? "column" : "row",
+          margin: 10,
+        }}
+      >
+        <EditDecisionButton onClick={onClickEdit} />
+        <CreateNewDecisionButton onClick={onClickCreateNew} />
+        {type == "random" && <RandomAskAgainButton />}
+      </div>
+    </div>
+  );
 }
--- src/features/result/result-hooks.ts
+++ src/features/result/result-hooks.ts
@@ -1,19 +1,11 @@
-import { ParsedUrlQuery } from "querystring";
-import { useEffect, useState } from "react";
-import { IWeightedResult, pushQuery } from "../../common/types/decision-types";
-import randomDecisionApi from "../randomDecision/randomDecision-api";
+import { useRouter } from "next/router";
+import { DecisionTypes } from "../../common/types/decision-types";
 
-export default function useResult(query: ParsedUrlQuery) {
-  const [weightedResult, setWeightedResult] = useState<IWeightedResult[]>([]);
-  const queryData: pushQuery["query"]["data"] = JSON.parse(
-    query.data as string
-  );
-  const id = queryData.id;
-  const decisionName = queryData.dec;
-
-  useEffect(() => {
-    randomDecisionApi.decide(id).then((val) => setWeightedResult(val));
-  }, []);
-
-  return { weightedResult, decisionName };
+export default function useResult() {
+  const router = useRouter();
+  const onClickEdit = () =>
+    router.push({ pathname: `/edit/${router.query.type}/${router.query.id}` });
+  const onClickCreateNew = () =>
+    router.push({ pathname: `/${router.query.type}` });
+  return { onClickEdit, onClickCreateNew };
 }
