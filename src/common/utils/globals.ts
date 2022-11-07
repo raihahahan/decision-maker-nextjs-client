b30a9d6326a4e999a873ac6edd9d977d3f531904
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-07 16:33:14 +0800

    minor change

--- src/common/utils/globals.ts
+++ src/common/utils/globals.ts
@@ -2,6 +2,7 @@ import { ButtonProps } from "@mantine/core";
 import { DecisionTypes } from "../types/decision-types";
 
 export const API_URL = "https://localhost:7239";
+
 export const DecisionTypeItems: {
   name: DecisionTypes;
   displayName: string;
