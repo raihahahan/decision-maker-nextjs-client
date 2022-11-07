fa6502f209b5152204cf65288c07214a53b61196
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-07 16:36:26 +0800

    modified choiceForm-types.ts

--- src/features/choiceForm/choiceForm-types.ts
+++ src/features/choiceForm/choiceForm-types.ts
@@ -1,5 +1,18 @@
+import { UseFormReturnType } from "@mantine/form";
+
 export type formHelpers = {
   removeChoice(id: number): void;
   addChoice(): void;
   decide(): void;
+  cancel(): void;
+};
+
+export type formHookReturnType<T> = {
+  form: UseFormReturnType<T, (values: T) => T>;
+  formHelpers: {
+    removeChoice(id: number): void;
+    addChoice(): void;
+    decide(): void;
+    cancel(): void;
+  };
 };
