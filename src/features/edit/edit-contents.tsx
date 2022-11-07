6367486981abd4b35143300d75b371d8921c8088
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-07 16:37:28 +0800

    modified edit contents to use the new implementation of form hook

--- src/features/edit/edit-contents.tsx
+++ src/features/edit/edit-contents.tsx
@@ -1,13 +1,17 @@
 import { IDecision } from "../../common/types/decision-types";
 import ChoicesForm from "../choiceForm/choiceForm-components";
+import useChoiceForm from "../choiceForm/choiceForm-hooks";
 import useEdit from "./edit-hooks";
 
-export default function EditContents({ res }: { res: IDecision }) {
-  const { onSubmit } = useEdit();
+export default function EditContents<T extends IDecision>({ res }: { res: T }) {
+  const { onSubmit, validate } = useEdit();
+
+  const editForm = useChoiceForm<T>(res, validate as any);
   return (
     <div>
       <h1>Edit</h1>
       <ChoicesForm
+        useChoiceForm={editForm}
         onSubmit={async (value: IDecision) => await onSubmit(value)}
         presetData={res}
         showBackButton
