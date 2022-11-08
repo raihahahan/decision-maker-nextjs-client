33a77cbdfc5c53797694038a147a1b5b096bbf43
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-08 18:16:43 +0800

    changed most DAL methods to implemented instead of abstract

--- src/common/domains/DAL.ts
+++ src/common/domains/DAL.ts
@@ -1,13 +1,86 @@
 import { IDecision, IFinalResult } from "../types/decision-types";
-
+import axios from "axios";
+import { API_URL } from "../utils/globals";
 // DAL: Data Access Layer
 
-abstract class DAL<T> {
-  abstract get(): Promise<T[]>;
-  abstract getById(id: number): Promise<T>;
-  abstract post(input: IDecision): Promise<IDecision>;
-  abstract put(id: number, value: IDecision): Promise<void>;
-  abstract decide(id: number): Promise<IFinalResult>;
+abstract class DAL<T extends IDecision> {
+  https = require("https");
+  agent = new this.https.Agent({
+    rejectUnauthorized: false,
+  });
+  route: string = "";
+
+  constructor(route: string) {
+    this.route = route;
+  }
+
+  public async get(): Promise<T[]> {
+    try {
+      const res = await axios.get(`${this.route}`);
+      return res.data as T[];
+    } catch (error) {
+      // alert(error);
+      return [];
+    }
+  }
+
+  public async getById(id: number): Promise<T> {
+    try {
+      const res = await fetch(`${this.route}/${id}`, {
+        method: "GET",
+        mode: "cors",
+        agent: this.agent,
+      } as any);
+      const jsonData: T = await res.json();
+      return jsonData;
+    } catch (error) {
+      return { name: "", choices: [] } as any;
+    }
+  }
+
+  public async put(id: number, value: T): Promise<void> {
+    try {
+      await axios.put(`${this.route}/${id}`, value);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  public async post(input: T): Promise<T> {
+    try {
+      delete input?.id;
+      for (let item of input.choices) {
+        delete item?.id;
+      }
+      const res = await axios.post(`${this.route}`, input);
+      return res.data as T;
+    } catch (error) {
+      // alert(error);
+      return { name: JSON.stringify(error), choices: [] } as any;
+    }
+  }
+
+  public async decide(id: number, requestBody?: any): Promise<IFinalResult> {
+    try {
+      const res = await fetch(`${this.route}/${id}/decide`, {
+        method: "POST",
+        mode: "cors",
+        agent: this.agent,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: requestBody ? JSON.stringify(requestBody) : undefined,
+      } as any);
+      const jsonData: IFinalResult = await res.json();
+      return jsonData;
+    } catch (error) {
+      return {
+        decisionName: JSON.stringify(error),
+        weightedResults: [],
+        type: "error",
+      };
+    }
+  }
 }
 
 export default DAL;
