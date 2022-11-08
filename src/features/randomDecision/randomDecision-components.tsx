b7750720b016c1d886816065652d445b77a7f32d
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-08 10:57:33 +0800

    added randomForm hook

--- src/features/randomDecision/randomDecision-components.tsx
+++ src/features/randomDecision/randomDecision-components.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@mantine/core";
 import { useRouter } from "next/router";
 import { DecisionTitle } from "../../common/components/branding";
+import { IDecision } from "../../common/types/decision-types";
 import ChoicesForm from "../choiceForm/choiceForm-components";
 import { InitialValidate, InitialValues } from "../choiceForm/choiceForm-data";
 import useChoiceForm from "../choiceForm/choiceForm-hooks";
@@ -12,7 +13,7 @@ export function RandomDecisionTitle() {
 
 export function RandomDecisionForm() {
   const { onSubmit } = useRandomDecision();
-  const randomForm = useChoiceForm(InitialValues, InitialValidate);
+  const randomForm = useChoiceForm<IDecision>(InitialValues, InitialValidate);
   return <ChoicesForm useChoiceForm={randomForm} onSubmit={onSubmit} />;
 }
 
