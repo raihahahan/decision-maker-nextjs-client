ffa8bd6b10f3dc21f40830920e30c1c0df26bb60
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-13 11:40:44 +0800

    modified routes

--- src/features/result/result-hooks.ts
+++ src/features/result/result-hooks.ts
@@ -4,7 +4,7 @@ import { DecisionTypes } from "../../common/types/decision-types";
 export default function useResult(type: DecisionTypes) {
   const router = useRouter();
   const onClickEdit = () =>
-    router.push({ pathname: `/edit/${type}/${router.query.id}` });
-  const onClickCreateNew = () => router.push({ pathname: `/${type}` });
+    router.push({ pathname: `/${type}/${router.query.id}/edit` });
+  const onClickCreateNew = () => router.push({ pathname: `/${type}/create` });
   return { onClickEdit, onClickCreateNew };
 }
