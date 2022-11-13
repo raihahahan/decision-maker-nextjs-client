d56efa68ac89153f68843ec970a395088e81fe61
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-13 11:42:07 +0800

    added random result page

--- src/pages/random/[id]/result.tsx
+++ src/pages/random/[id]/result.tsx
@@ -1,8 +1,19 @@
-export default function RandomResultPage() {
-  return (
-    <h1>
-      Random Result page. This shows the final result. There is an edit button,
-      and ask another question, and decide button.
-    </h1>
-  );
+import { NextPageContext } from "next";
+import { useEffect } from "react";
+import { IFinalResult } from "../../../common/types/decision-types";
+import randomDecisionApi from "../../../features/randomDecision/randomDecision-api";
+import ResultContents from "../../../features/result/result-contents";
+
+export default function RandomResultPage({ res }: { res: IFinalResult }) {
+  return <ResultContents data={res} type="random" />;
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const id: string = context.query.id as string;
+  const res = await randomDecisionApi.decide(+id);
+  return {
+    props: {
+      res,
+    }, // will be passed to the page component as props
+  };
 }
