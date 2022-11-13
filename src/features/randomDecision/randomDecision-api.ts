9d08b793315ab56bdf42735e47c8851b4ec1e06a
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-13 11:39:39 +0800

    added random choice api

--- src/features/randomDecision/randomDecision-api.ts
+++ src/features/randomDecision/randomDecision-api.ts
@@ -1,11 +1,21 @@
 import { IRandomDecisionItem } from "./randomDecision-types";
 import { API_URL } from "../../common/utils/globals";
+import DecisionDAL from "../../common/domains/DecisionDAL";
+import { IChoice } from "../../common/types/decision-types";
 import DAL from "../../common/domains/DAL";
 
-class RandomDecisionAPI extends DAL<IRandomDecisionItem> {
+class RandomDecisionAPI extends DecisionDAL<IRandomDecisionItem> {
   constructor() {
     super(`${API_URL}/api/RandomDecisionItems`);
   }
 }
 
+class RandomChoiceAPI extends DAL<IChoice> {
+  constructor() {
+    super(`${API_URL}/api/RandomChoices`);
+  }
+}
+
+const randomChoiceApi = new RandomChoiceAPI();
 export default new RandomDecisionAPI();
+export { randomChoiceApi };
