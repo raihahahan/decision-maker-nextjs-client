cda02c468118325eca3f35186fa1c169b74a0faf
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-13 11:38:31 +0800

    modified routes

--- src/common/utils/globals.ts
+++ src/common/utils/globals.ts
@@ -1,10 +1,11 @@
 import { ButtonProps } from "@mantine/core";
+import { routes } from "../../features/site/site-types";
 import { DecisionTypes } from "../types/decision-types";
 
 export const API_URL = "https://localhost:7239";
 
 export type DecisionTypeItemsType = {
-  name: DecisionTypes;
+  name: routes;
   displayName: string;
   color: ButtonProps["color"];
   description: string;
@@ -12,21 +13,21 @@ export type DecisionTypeItemsType = {
 
 export const DecisionTypeItems: DecisionTypeItemsType[] = [
   {
-    name: "random",
+    name: "/random/create",
     displayName: "Random",
     color: "cyan",
     description:
       "Provide a list of choices. This will randomly pick one choice as the final decision.",
   },
   {
-    name: "conditional",
+    name: "/conditional/create",
     displayName: "Conditional",
     color: "blue",
     description:
       "From a list of choices, provide a list of binary conditions affecting your decision.",
   },
   {
-    name: "weighted",
+    name: "/weighted/create",
     displayName: "Weighted",
     color: "violet",
     description:
