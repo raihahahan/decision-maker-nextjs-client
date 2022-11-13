ad813e3151ea773d1a8988a30c5fc8eb398d3a43
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-13 11:42:28 +0800

    added random edit page

--- src/pages/random/[id]/edit.tsx
+++ src/pages/random/[id]/edit.tsx
@@ -1,3 +1,19 @@
-export default function RandomEditPage() {
-  return <h1>Random Edit page.</h1>;
+import { NextPageContext } from "next";
+import { IFinalResult } from "../../../common/types/decision-types";
+import randomDecisionApi from "../../../features/randomDecision/randomDecision-api";
+import { RandomDecisionEditFormContents } from "../../../features/randomDecision/randomDecision-contents";
+import { IRandomDecisionItem } from "../../../features/randomDecision/randomDecision-types";
+
+export default function RandomEditPage({ res }: { res: IRandomDecisionItem }) {
+  return <RandomDecisionEditFormContents res={res} />;
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const id: string = context.query.id as string;
+  const presetValues = await randomDecisionApi.getById(+id);
+  return {
+    props: {
+      res: presetValues,
+    }, // will be passed to the page component as props
+  };
 }
