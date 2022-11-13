fb3abe4321a85a6bfe4bf937679956ea5df6aacf
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-13 11:40:05 +0800

    separated create and edit form for random decision

--- src/features/randomDecision/randomDecision-components.tsx
+++ src/features/randomDecision/randomDecision-components.tsx
@@ -1,29 +1,60 @@
 import { Button } from "@mantine/core";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DecisionTitle } from "../../common/components/branding";
 import usePreventExitForm from "../../common/hooks/usePreventExitForm";
-import { IDecision } from "../../common/types/decision-types";
+import { IDecision, IFinalResult } from "../../common/types/decision-types";
 import ChoicesForm from "../choiceForm/choiceForm-components";
 import { InitialValidate, InitialValues } from "../choiceForm/choiceForm-data";
 import useChoiceForm from "../choiceForm/choiceForm-hooks";
 import useRandomDecision from "./randomDecision-hooks";
+import { IRandomDecisionItem } from "./randomDecision-types";
 
 export function RandomDecisionTitle() {
   return <DecisionTitle title="Random Decision" />;
 }
 
-export function RandomDecisionForm() {
+export function RandomDecisionCreateForm() {
   const [unsavedChanges, setUnsavedChanges] = useState(true);
-
   usePreventExitForm(unsavedChanges);
-  const { onSubmit } = useRandomDecision();
+  const { createHandlers } = useRandomDecision();
   const randomForm = useChoiceForm<IDecision>(
     InitialValues,
     InitialValidate,
     setUnsavedChanges
   );
-  return <ChoicesForm useChoiceForm={randomForm} onSubmit={onSubmit} />;
+  return (
+    <ChoicesForm
+      useChoiceForm={randomForm}
+      onSubmit={createHandlers.onSubmitCreate}
+    />
+  );
+}
+
+export function RandomDecisionEditForm({ res }: { res: IRandomDecisionItem }) {
+  const [unsavedChanges, setUnsavedChanges] = useState(true);
+  usePreventExitForm(unsavedChanges);
+  const { editHandlers } = useRandomDecision();
+  const presetData: IRandomDecisionItem = {
+    id: res.id,
+    name: res.name,
+    choices: res.choices,
+  };
+
+  const randomEditForm = useChoiceForm<IDecision>(
+    presetData,
+    InitialValidate,
+    setUnsavedChanges,
+    editHandlers
+  );
+
+  return (
+    <ChoicesForm
+      useChoiceForm={randomEditForm}
+      onSubmit={editHandlers.onSubmitEdit}
+      presetData={presetData}
+    />
+  );
 }
 
 export function RandomAskAgainButton() {
