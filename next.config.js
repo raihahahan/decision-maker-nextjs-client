1339238ff0cec641d3082f63ce998e9363a58c2c
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-13 11:36:46 +0800

    removed redirect

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/about",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
