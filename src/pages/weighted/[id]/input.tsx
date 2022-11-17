f8857b88f8dc0193308f4028db01055b52510449
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-18 07:07:07 +0800

    added weighted input page

--- src/pages/weighted/[id]/input.tsx
+++ src/pages/weighted/[id]/input.tsx
@@ -1,8 +1,22 @@
-export default function WeightedInputPage() {
-  return (
-    <h1>
-      Weighted Input page. This is where users give input for Weighted decision.
-      There is a Decide button.
-    </h1>
-  );
+import { NextPageContext } from "next";
+import { useEffect } from "react";
+import Error from "../../../common/components/error";
+import { WeightedInputContents } from "../../../features/weightedDecision/weightedDecision-contents";
+import { IWeightedDecisionItem } from "../../../features/weightedDecision/weightedDecision-types";
+import weightedDeicisonApi from "../../../features/weightedDecision/weightedDeicison-api";
+
+export default function WeightedInputPage({
+  res,
+}: {
+  res: IWeightedDecisionItem;
+  id: number;
+}) {
+  if (!res?.choices) return <Error />;
+  return <WeightedInputContents res={res} />;
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const id: number = context.query.id ? +context.query.id : -1;
+  const res = await weightedDeicisonApi.getById(id);
+  return { props: { res } };
 }
