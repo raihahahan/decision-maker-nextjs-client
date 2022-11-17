65b8beedfc5599af8c6927e05dc2c251ca56e5da
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-18 07:07:42 +0800

    modified weighted api layer

--- src/features/weightedDecision/weightedDeicison-api.ts
+++ src/features/weightedDecision/weightedDeicison-api.ts
@@ -1,11 +1,24 @@
 import DAL from "../../common/domains/DAL";
+import DecisionDAL from "../../common/domains/DecisionDAL";
 import { API_URL } from "../../common/utils/globals";
-import { IWeightedDecisionItem } from "./weightedDecision-types";
+import {
+  IWeightedDecisionItem,
+  IWeightedInputItem,
+} from "./weightedDecision-types";
 
-class WeightedDecisionAPI extends DAL<IWeightedDecisionItem> {
+class WeightedDecisionAPI extends DecisionDAL<IWeightedDecisionItem> {
   constructor() {
     super(`${API_URL}/api/WeightedDecisionItems`);
   }
 }
 
+class WeightedInputAPI extends DAL<IWeightedInputItem> {
+  constructor() {
+    super(`${API_URL}/api/WeightedInputItems`);
+  }
+}
+
+const weightedInputApi = new WeightedInputAPI();
+
 export default new WeightedDecisionAPI();
+export { weightedInputApi };
