069b3d8f3407bdc1bbe6cff29dd8eee1434e2f27
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-23 11:46:00 +0800

    added Criteria class

--- src/features/weightedDecision/weightedDecision-data.ts
+++ src/features/weightedDecision/weightedDecision-data.ts
@@ -1,5 +1,6 @@
 import { InitialValidate, InitialValues } from "../choiceForm/choiceForm-data";
 import {
+  ICriteria,
   IWeightedDecisionItem,
   IWeightedInput,
 } from "./weightedDecision-types";
@@ -18,3 +19,34 @@ export const initialWeightedValues: IWeightedDecisionItem = {
 export const initialWeightedValidate = {
   ...InitialValidate,
 };
+
+export class Criteria implements ICriteria {
+  name = "";
+  id: number | undefined = undefined;
+  weight = 0;
+  decisionId: number | undefined = undefined;
+  value = 20;
+
+  constructor(
+    name?: string,
+    id?: number,
+    decisionId?: number,
+    weight?: number,
+    value?: number
+  ) {
+    this.name = name ?? "";
+    this.id = id;
+    this.decisionId = decisionId;
+    this.weight = weight ?? 0;
+    this.value = value ?? 20;
+  }
+
+  get() {
+    return this.name;
+  }
+
+  set(value: string, weight: number) {
+    this.name = value;
+    this.weight = weight;
+  }
+}
