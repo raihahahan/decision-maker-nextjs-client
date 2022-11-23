3c6671192bf8308a3baaa7cb4cffb1e51faffbe9
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-23 11:52:16 +0800

    added weighteddecision reducer

--- src/redux/combinedReducers.ts
+++ src/redux/combinedReducers.ts
@@ -1,14 +1,14 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import randomDecisionReducer from "../features/randomDecision/randomDecision-slice";
 import themeReducer from "../features/theme/theme-slice";
-import weightedInputReducer from "../features/weightedDecision/weightedDecision-slice";
+import weightedDecisionReducer from "../features/weightedDecision/weightedDecision-slice";
 
 const decisionReducer = combineReducers({
   randomDecision: randomDecisionReducer,
+  weightedDecision: weightedDecisionReducer,
 });
 
 export const rootReducer = combineReducers({
   theme: themeReducer,
-  weightedInput: weightedInputReducer,
   decision: decisionReducer,
 });
