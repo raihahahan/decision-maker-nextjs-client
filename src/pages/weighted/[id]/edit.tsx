ca66b7a45bfa5d0824a1cd48abcff14ad6cb2aee
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-23 11:49:34 +0800

    added weighted edit page

--- src/pages/weighted/[id]/edit.tsx
+++ src/pages/weighted/[id]/edit.tsx
@@ -1,3 +1,22 @@
-export default function WeightedEditPage() {
-  return <h1>Weighted Edit page.</h1>;
+import { NextPageContext } from "next";
+import { WeightedEditContents } from "../../../features/weightedDecision/weightedDecision-contents";
+import { IWeightedDecisionItem } from "../../../features/weightedDecision/weightedDecision-types";
+import weightedDeicisonApi from "../../../features/weightedDecision/weightedDeicison-api";
+
+export default function WeightedEditPage({
+  res,
+}: {
+  res: IWeightedDecisionItem;
+}) {
+  return <WeightedEditContents res={res} />;
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const id: string = context.query.id as string;
+  const res = await weightedDeicisonApi.getById(+id);
+  return {
+    props: {
+      res,
+    }, // will be passed to the page component as props
+  };
 }
