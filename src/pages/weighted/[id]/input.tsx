75f5207f6447bf188bce262b7d8fe17d0ecca32a
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-23 11:51:28 +0800

    modified server side code for weighted input page

--- src/pages/weighted/[id]/input.tsx
+++ src/pages/weighted/[id]/input.tsx
@@ -1,22 +1,33 @@
 import { NextPageContext } from "next";
-import { useEffect } from "react";
 import Error from "../../../common/components/error";
 import { WeightedInputContents } from "../../../features/weightedDecision/weightedDecision-contents";
-import { IWeightedDecisionItem } from "../../../features/weightedDecision/weightedDecision-types";
-import weightedDeicisonApi from "../../../features/weightedDecision/weightedDeicison-api";
+import {
+  IWeightedDecisionItem,
+  IWeightedInput,
+  IWeightedInputItem,
+} from "../../../features/weightedDecision/weightedDecision-types";
+import weightedDeicisonApi, {
+  weightedInputApi,
+} from "../../../features/weightedDecision/weightedDeicison-api";
 
 export default function WeightedInputPage({
   res,
+  weightedInput,
 }: {
   res: IWeightedDecisionItem;
-  id: number;
+  weightedInput?: IWeightedInputItem;
 }) {
   if (!res?.choices) return <Error />;
-  return <WeightedInputContents res={res} />;
+  return <WeightedInputContents res={res} weightedInput={weightedInput} />;
 }
 
 export async function getServerSideProps(context: NextPageContext) {
   const id: number = context.query.id ? +context.query.id : -1;
   const res = await weightedDeicisonApi.getById(id);
-  return { props: { res } };
+  let weightedInput = undefined;
+  if (await weightedInputApi.exists(id)) {
+    weightedInput = await weightedInputApi.getById(id);
+  }
+
+  return { props: { res, weightedInput: weightedInput ?? null } };
 }
