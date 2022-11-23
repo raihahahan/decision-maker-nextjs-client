86a9223d538b64ed9b65021869df2804b337ef9f
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-23 11:47:50 +0800

    added reducer for weighted decision list

--- src/features/weightedDecision/weightedDecision-slice.ts
+++ src/features/weightedDecision/weightedDecision-slice.ts
@@ -1,22 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { IWeightedInput } from "./weightedDecision-types";
+import { TDecisionReducerState } from "../../common/types/decision-types";
+import { IWeightedDecisionItem } from "./weightedDecision-types";
 
-export const weightedInputSlice = createSlice({
-  name: "weightedInput",
-  initialState: [] as IWeightedInput[],
+export const weightedDecisionSlice = createSlice({
+  name: "weightedDecision",
+  initialState: [] as IWeightedDecisionItem[],
   reducers: {
-    setWeightedInput: (
-      state: IWeightedInput[],
-      action: { payload: IWeightedInput[] }
+    setWeightedDecision: (
+      state: IWeightedDecisionItem[],
+      action: { payload: IWeightedDecisionItem[] }
     ) => {
       return action.payload;
+    },
+    addWeightedDecision: (
+      state: IWeightedDecisionItem[],
+      action: { payload: IWeightedDecisionItem }
+    ) => {
+      return [...state, action.payload];
+    },
+    updateWeightedDecision: (
+      state: IWeightedDecisionItem[],
+      action: { payload: { id: number; data: IWeightedDecisionItem } }
+    ) => {
+      return state.map((item) => {
+        if (item?.id && item.id == action.payload.id) {
+          return action.payload.data;
+        } else return item;
+      });
+    },
+    removeWeightedDecision: (
+      state: IWeightedDecisionItem[],
+      action: { payload: number }
+    ) => {
+      return state.filter((item) => item?.id != action.payload);
     },
   },
 });
 
-export const { setWeightedInput } = weightedInputSlice.actions;
-export const selectWeightedInput = (state: {
-  weightedInput: IWeightedInput[];
-}): IWeightedInput[] => state.weightedInput;
-const weightedInputReducer = weightedInputSlice.reducer;
-export default weightedInputReducer;
+export const {
+  setWeightedDecision,
+  addWeightedDecision,
+  updateWeightedDecision,
+  removeWeightedDecision,
+} = weightedDecisionSlice.actions;
+export const selectWeightedDecision = (
+  state: TDecisionReducerState
+): IWeightedDecisionItem[] => state.decision.weightedDecision;
+const weightedDecisionReducer = weightedDecisionSlice.reducer;
+export default weightedDecisionReducer;
