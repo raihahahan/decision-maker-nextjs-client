104a85c86d5fc9fac3c80d076a1e36046d154641
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-23 11:48:03 +0800

    added extra form config type for weighted decision

--- src/features/weightedDecision/weightedDecision-types.ts
+++ src/features/weightedDecision/weightedDecision-types.ts
@@ -1,4 +1,5 @@
 import { IDecision } from "../../common/types/decision-types";
+import { TExtraFormConfig } from "../choiceForm/choiceForm-types";
 
 export interface ICriteria {
   id?: number;
@@ -8,8 +9,9 @@ export interface ICriteria {
 }
 
 export interface ICriteriaInput extends ICriteria {
-  value: number;
   inputId?: number; // Foreign key references weightedInput
+  criteriaId?: number;
+  value: number;
 }
 
 export interface IWeightedInput {
@@ -28,4 +30,11 @@ export interface IWeightedInputItem {
 
 export interface IWeightedDecisionItem extends IDecision {
   criteriaList: ICriteria[];
+}
+
+export interface IExtraFormConfig<T, C> extends TExtraFormConfig<T> {
+  onAddCriteria(decisionId: number): Promise<number>;
+  onRemoveCriteria(id: number): Promise<void>;
+  onEditCriteria(id: number, value: C, isEditSlider?: boolean): Promise<void>;
+  onSubmitEdit(value: any): Promise<void>;
 }
