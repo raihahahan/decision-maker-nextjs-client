97d1f6be40e30d9f717636b8ef0be2b42a8c9568
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-30 14:33:13 +0800

    added refId for choice domain

--- src/common/domains/domains.ts
+++ src/common/domains/domains.ts
@@ -1,9 +1,11 @@
 import { IChoice } from "../types/decision-types";
+import { v4 as uuidv4 } from "uuid";
 
 export class Choice implements IChoice {
   name = "";
   id: number | undefined = undefined;
   decisionId: number | undefined = undefined;
+  refId: string = uuidv4();
 
   constructor(name?: string, id?: number, decisionId?: number) {
     this.name = name ?? "";
