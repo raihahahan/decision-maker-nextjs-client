cb1518db08569a76572b05b5cd9caef5204fe7ed
Muhd Raihan <muhdraihan1140@gmail.com>
2022-11-30 14:32:57 +0800

    added edit page for conditional Decision

--- src/pages/conditional/[id]/index.tsx
+++ src/pages/conditional/[id]/index.tsx
@@ -1,7 +1,18 @@
-export default function ConditionalIndexPage() {
-  <h1>
-    Conditional Index page. This page shows the list of choices and name of
-    decision. There is a button to go to Edit page. There is another button to
-    go to Input page.
-  </h1>;
+import { NextPageContext } from "next";
+import conditionalDecisionApi from "../../../features/conditionalDecision/conditionalDecision-api";
+import { ConditionalDecisionEditContents } from "../../../features/conditionalDecision/conditionalDecision-contents";
+import { IConditionalDecisionItem } from "../../../features/conditionalDecision/conditionalDecision-types";
+
+export default function ConditionalIndexPage({
+  res,
+}: {
+  res: IConditionalDecisionItem;
+}) {
+  return <ConditionalDecisionEditContents res={res} />;
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const id = +(context.query.id as string);
+  const res = await conditionalDecisionApi.getById(id);
+  return { props: { res } };
 }
