1c9c26a86c2dc40b86a2cdee5a0b5ef2a139603f
Raihan Rizqullah <mraihandev@gmail.com>
2023-01-02 11:36:49 +0800

    one instance of axios: modified DAL.ts and DecisionDAL.ts domains

--- src/common/domains/DAL.ts
+++ src/common/domains/DAL.ts
@@ -1,62 +1,40 @@
-import axios from "axios";
-import { URL } from "url";
 // DAL: Data Access Layer
 
-abstract class DAL<T> {
-  protected https = require("https");
-  protected agent = new this.https.Agent({
-    rejectUnauthorized: false,
-  });
-  protected route: string = "";
+import { makeUrlWithQueries, pathCombine } from "../utils/api-utils";
+import { axios } from "../api/Axios";
+import { TDALParams } from "../types/decision-types";
 
-  constructor(route: string) {
-    this.route = route;
+abstract class DAL<T> {
+  /*
+    Patterns:
+    - API_URL/{decisionType}/{id}/{action}
+    - API_URL/{decisionType}/{action}
+  */
+
+  protected decisionType: string;
+
+  constructor(decisionType: string) {
+    this.decisionType = decisionType;
   }
 
-  public async get(params?: {
-    sortorder?: string;
-    pageNumber?: number;
-    q?: string;
-  }): Promise<T[]> {
+  public async get(params?: TDALParams): Promise<T[]> {
     try {
-      let url = this.route;
-      let query = "";
+      let endpoint : string = pathCombine(false, this.decisionType);
       if (params) {
-        query = "?";
-        let queriesArr = [];
-        for (const [k, v] of Object.entries(params)) {
-          if (v != undefined) {
-            queriesArr.push(k + "=" + v);
-          }
-        }
-        query += queriesArr.join("&");
-        url += query;
+        endpoint = makeUrlWithQueries(endpoint, params)
       }
-
-      const res = await fetch(`${url}`, {
-        method: "GET",
-        mode: "cors",
-        agent: this.agent,
-      } as any);
-
-      const jsonData: T[] = await res.json();
-      return jsonData;
+      const res = await axios.get<any, T[]>(endpoint);
+      return res;
     } catch (error) {
-      // alert(error);
       return [];
     }
   }
 
   public async getById(id: number): Promise<T> {
     try {
-      const res = await fetch(`${this.route}/${id}`, {
-        method: "GET",
-        mode: "cors",
-        agent: this.agent,
-      } as any);
-
-      const jsonData: T = await res.json();
-      return jsonData;
+      const endpoint = pathCombine(false, this.decisionType, id.toString());
+      const res = await axios.get<any, T>(endpoint);
+      return res;
     } catch (error) {
       return null as any;
     }
@@ -64,7 +42,8 @@ abstract class DAL<T> {
 
   public async put<TVal>(id: number, value: TVal): Promise<void> {
     try {
-      await axios.put(`${this.route}/${id}`, value);
+      const endpoint = pathCombine(false, this.decisionType, id.toString());
+      await axios.put(endpoint, value);
     } catch (error) {
       alert(error);
     }
@@ -72,17 +51,18 @@ abstract class DAL<T> {
 
   public async post(input: T): Promise<T> {
     try {
-      const res = await axios.post(`${this.route}`, input);
+      const endpoint = pathCombine(false, this.decisionType);
+      const res = await axios.post(endpoint, input);
       return res.data as T;
     } catch (error) {
-      // alert(error);
       return null as any;
     }
   }
 
   public async delete(id: number): Promise<void> {
     try {
-      await axios.delete(`${this.route}/${id}`);
+      const endpoint = pathCombine(false, this.decisionType, id.toString());
+      await axios.delete(endpoint);
     } catch (error) {
       return;
     }
@@ -96,7 +76,6 @@ abstract class DAL<T> {
       }
       return true;
     } catch (error) {
-      alert(error);
       return false;
     }
   }
--- src/common/domains/DecisionDAL.ts
+++ src/common/domains/DecisionDAL.ts
@@ -1,6 +1,8 @@
 import { IDecision, IFinalResult } from "../types/decision-types";
 import DAL from "./DAL";
-import axios from "axios";
+import { axios } from "../api/Axios";
+import { pathCombine } from "../utils/api-utils";
+import CONSTANTS from "../utils/constants";
 
 class DecisionDAL<T extends IDecision> extends DAL<T> {
   public override async post(input: T): Promise<T> {
@@ -9,30 +11,22 @@ class DecisionDAL<T extends IDecision> extends DAL<T> {
       for (let item of input.choices) {
         delete item?.id;
       }
-      const res = await axios.post(`${this.route}`, input);
-      return res.data as T;
+      const endpoint = pathCombine(false, this.decisionType);
+      const res = await axios.post<any, T>(endpoint, input);
+      return res;
     } catch (error) {
-      // alert(error);
-      return { name: JSON.stringify(error), choices: [] } as any;
+      return { name: null, choices: [] } as any;
     }
   }
 
   public async decide<R>(id: number, requestBody?: R): Promise<IFinalResult> {
     try {
-      const res = await fetch(`${this.route}/${id}/decide`, {
-        method: "POST",
-        mode: "cors",
-        agent: this.agent,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: requestBody ? JSON.stringify(requestBody) : undefined,
-      } as any);
-      const jsonData: IFinalResult = await res.json();
-      return jsonData;
+      const endpoint = pathCombine(false, this.decisionType, id.toString(), CONSTANTS.ACTION_METHODS.DECIDE);
+      const res = await axios.post<any, IFinalResult>(endpoint, requestBody ?? undefined);
+      return res;
     } catch (error) {
       return {
-        decisionName: JSON.stringify(error),
+        decisionName: "",
         weightedResults: [],
         type: "error",
       };
@@ -41,12 +35,9 @@ class DecisionDAL<T extends IDecision> extends DAL<T> {
 
   public async totalPages(): Promise<number> {
     try {
-      const res = await fetch(`${this.route}/totalPages`, {
-        method: "GET",
-        mode: "cors",
-        agent: this.agent,
-      } as any);
-      return res.json();
+      const endpoint = pathCombine(false, this.decisionType, CONSTANTS.ACTION_METHODS.TOTAL_PAGES);
+      const res = await axios.get<any, number>(endpoint);
+      return res;
     } catch (error) {
       return 1;
     }
